b3dc448683327c4cf15a74ea59bc8dd229e51ef0
Anthony Sá Reis <102253239+AnthonySaReis@users.noreply.github.com>
2022-10-18 19:03:55 -0300

    Update 41.c

--- Lista1-C/41.c
+++ Lista1-C/41.c
@@ -8,8 +8,9 @@ calculado.
 
 int main(){
     float valor, hrs, total;
+   	printf("Entre com os valores das horas trabalhadas e o valor da hora separado por espaco: ");
     scanf("%f %f", &valor, &hrs);
     total = (valor * hrs) * 1.1;//adicionando 10% sobre o valor calculado.
-    printf("%0.2f", total);
+    printf("valor a receber %0.2f", total);
     return 0;
 }
