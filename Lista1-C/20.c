4882c1f5ef977dcb4f1e17d090f46d614f4e26b1
Anthony Sá Reis <102253239+AnthonySaReis@users.noreply.github.com>
2022-10-18 19:17:39 -0300

    Update 20.c

--- Lista1-C/20.c
+++ Lista1-C/20.c
@@ -8,8 +8,8 @@ conversão é: L = K/0.45, sendo K a massa em quilogramas e L a massa em libras.
 
 int main( ) {
 	float k,l;
-	scanf("%f", &k);
 	printf("Entre com o valor em quilogramas: ");
+	scanf("%f", &k);
 	l = k/0.45;
 	printf("Em libras vale: %0.2f", l);
 	return 0;
