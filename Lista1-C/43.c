0ff861d6fc239002e9a651c556c0b1dd06025fe4
Anthony Sá Reis <102253239+AnthonySaReis@users.noreply.github.com>
2022-10-18 19:08:39 -0300

    Update 43.c

--- Lista1-C/43.c
+++ Lista1-C/43.c
@@ -10,14 +10,15 @@
 int main(){
 
     float valor, desconto, parcela, comissaoVista, comissaoParcela;
+	printf("Entre com o valor total: ");
     scanf("%f", &valor);
     desconto = valor * 0.9; //desconto de 10%
-    printf("%0.2f\n", desconto);
+    printf("valor com desconto %0.2f\n", desconto);
     parcela = valor/3; //o valor de cada parcela, no parcelamento de 3x sem juros
-    printf("%0.2f\n", parcela);
+    printf("valor de cada parcela %0.2f\n", parcela);
     comissaoVista = desconto * 1.05; //a comissão do vendedor, no caso da venda ser a vista (5% sobre o valor com desconto)
-    printf("%0.2f\n", comissaoVista);
+    printf("comissao em caso de compra a vista %0.2f\n", comissaoVista);
     comissaoParcela = valor * 1.05; //a comissão do vendedor, no caso da venda ser parcelada (5% sobre o valor total)
-    printf("%0.2f", comissaoParcela);
+    printf("comissao em caso de compra parcelada %0.2f", comissaoParcela);
     return 0;
 }
