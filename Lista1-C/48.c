e54e3a8895cdb1c64a4892da7cca88b269fd6f94
Anthony Sá Reis <102253239+AnthonySaReis@users.noreply.github.com>
2022-10-18 19:13:29 -0300

    Update 48.c

--- Lista1-C/48.c
+++ Lista1-C/48.c
@@ -7,11 +7,12 @@
 int main(){
 
     int horas, minutos, segundos;
+	printf("Entre com o valor inteiro em segundos: ");
     scanf("%d", &segundos);
     horas = segundos/3600;
     minutos = (segundos/60) - (horas*60); //para eliminar as horas e deixar somente os minutos
     segundos = segundos%60; //recebe os segundos que ficaram para liberar minutos
-    printf("%dh, %dm e %ds\n", horas, minutos, segundos);
+    printf("%d h, %d m e %d s\n", horas, minutos, segundos);
 
     return 0;
 }
