36ef9e36238a6c57906a896b9cfa6d50f7641f3a
Anthony Sá Reis <102253239+AnthonySaReis@users.noreply.github.com>
2022-10-18 19:12:01 -0300

    Update 46.c

--- Lista1-C/46.c
+++ Lista1-C/46.c
@@ -7,12 +7,13 @@ outro número formado pelos dígitos invertidos do número lido
 int main(){
 
     int numero, centena, dezena, unidade, invertido;
+	printf("Entre com o valor inteiro de 3 digitos: ");
     scanf("%d", &numero);
     centena = numero/100; //para pegar o primeiro numero isolado
     dezena = (numero/10)%10; //para pegar o segundo numero isolado (divide por 10, para colocar a unidade e depois retirar a unidade de lá com o operador do resto)
     unidade =  numero%10; //para pegar o unico numero que não saiu nos outros (numero restante)
     invertido = (unidade*100) + (dezena*10) + centena; //para inverter centena com unidades, dezena com dezena e unidades com centenas... e, então, somar tudo msm
-    printf("%d\n", invertido);
+    printf("valor invertido: %d\n", invertido);
 
     return 0;
 }
