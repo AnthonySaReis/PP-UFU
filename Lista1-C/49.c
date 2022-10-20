1a15f8b4f550c6148ad5861a1c28ff945cc5e0cc
Anthony Sá Reis <102253239+AnthonySaReis@users.noreply.github.com>
2022-10-20 19:59:17 -0300

    Update 49.c

--- Lista1-C/49.c
+++ Lista1-C/49.c
@@ -3,6 +3,7 @@
 segundos, de uma experiência biológica. O programa deve resultar com o novo horário (hora,
 minuto e segundo) do termino da mesma
 */
+
 #include <stdio.h>
 
 int main(){
@@ -10,23 +11,21 @@ int main(){
     int horaInicial, minInicial, segInicial, horaFinal, minFinal, segFinal, durSegundos;
     printf("Entre com horas na primeira linha, minutos na segunda, segundos na terceira e a duracao em segundos na ultima: \n");
     scanf("%d\n%d\n%d\n %d", &horaInicial, &minInicial, &segInicial, &durSegundos);
-    
+
     segFinal = segInicial + durSegundos;
+    minFinal= minInicial;
     //controle de tempo para situações em que há mais de um minuto ou mais de uma hora na duração
-    if(segFinal>59){
-    	minFinal= minInicial+1;
-        segFinal= segFinal-60;
-        horaFinal = horaInicial;
-            if(minFinal>59){
-    	    horaFinal = horaInicial+1;
-            minFinal=minFinal-60;    }
-    } 
-    //caso em que so os segundos são interferidos
-    else {
-        minFinal=minInicial;
-        horaFinal=horaInicial;
-        segFinal = segInicial+durSegundos;
-    }
+    while(segFinal>59){
+        minFinal++;
+        segFinal -= 60;
+        horaFinal = horaInicial;}
+
+    if(minFinal>59){
+    horaFinal = horaInicial+1;
+    minFinal=minFinal-60;    }
+
+
+
     printf("Hora final: %d horas, %d minutos e %d segundos", horaFinal, minFinal, segFinal);
 
     return 0;
